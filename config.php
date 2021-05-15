a92c511d8db40a3981efe824af5443a3d9477bed
Rayyan Roshan Ahmed <83157095+Rayyan-Roshan-Ahmed@users.noreply.github.com>
2021-05-15 19:22:11 +0530

    Added remote mysql connection

--- config.php
+++ config.php
@@ -1,9 +1,16 @@
 <?php 
 
-$server="localhost";
-$username="root";
-$password="";
-$db="basic bank system Database";
+//Development connection
+//$server="localhost";
+//$username="root";
+//$password="";
+//$db="basic bank system Database";
+
+//Remote database connection
+$server="remotemysql.com";
+$username="QxiwlRmaiO";
+$password="xwUheNygDh";
+$db="QxiwlRmaiO";
 
 $conn=mysqli_connect($server,$username,$password,$db);
 
